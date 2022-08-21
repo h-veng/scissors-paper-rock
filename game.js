4316011ceb249996596dde06ea35e4ee271c9b92
h-veng <henry.veng@hotmail.com>
2022-08-21 20:47:27 +1000

    Add function to prompt for player choice

--- game.js
+++ game.js
@@ -1,6 +1,30 @@
 // Testing getComputerChoice()
 // console.log(getComputerChoice())
 
+// Testing getPlayerChocie()
+// console.log(getPlayerChoice())
+
+function getPlayerChoice() {
+    while (true) {
+        choice = prompt("Choose: scissors, paper, or rock")
+        choice = choice.toLowerCase()
+    
+        if (choice === "scissors") {
+            return "Scissors"
+        }
+    
+        if (choice === "paper") {
+            return "Paper"
+        }
+    
+        if (choice === "rock") {
+            return "Rock"
+        }
+
+        alert("Please type in 'Scissors', 'Paper', or 'Rock'")
+    }
+}
+
 function getComputerChoice() {
     randomNumber = Math.floor(Math.random() * 3);
     
