c658e748021c8cbd38cdd67eabf2e4514b785327
h-veng <henry.veng@hotmail.com>
2022-08-21 21:12:38 +1000

    Add logic to play one game

--- game.js
+++ game.js
@@ -4,6 +4,53 @@
 // Testing getPlayerChocie()
 // console.log(getPlayerChoice())
 
+let playerChoice = getPlayerChoice();
+let computerChoice = getComputerChoice();
+let outcomeMessage = "";
+let outcome;
+
+if (playerChoice === "Scissors") {
+    if (computerChoice === "Scissors") {
+        outcome = "tie";
+    } else if (computerChoice === "Paper") {
+        outcome = "win";
+    } else {
+        outcome = "lose";
+    }
+} else if (playerChoice === "Paper") {
+    if (computerChoice === "Scissors") {
+        outcome = "lose";
+    } else if (computerChoice === "Paper") {
+        outcome = "tie";
+    } else {
+        outcome = "win";
+    }
+} else {
+    if (computerChoice === "Scissors") {
+        outcome = "win";
+    } else if (computerChoice === "Paper") {
+        outcome = "lose";
+    } else {
+        outcome = "tie";
+    }
+}
+
+switch (outcome) {
+    case "win":
+        outcomeMessage = `You win! ${playerChoice} beats ${computerChoice}.`;
+        break;
+    case "lose":
+        outcomeMessage = `You lose! ${computerChoice} beats ${playerChoice}`;
+        break;
+    case "tie":
+        outcomeMessage = `It's a tie! You both chose ${playerChoice}.`;
+        break;
+    default:
+        outcomeMessage = "Invalid game.";
+}
+
+console.log(outcomeMessage);
+
 function getPlayerChoice() {
     while (true) {
         let choice = prompt("Choose: scissors, paper, or rock");
